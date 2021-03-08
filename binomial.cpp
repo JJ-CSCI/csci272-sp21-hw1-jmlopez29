8f8599c548f1623f904b660f53338ceaeb8259d8
jmlopez29 <jmlopez2992@gmail.com>
2021-03-08 01:48:45 +0000

    Zoom edits. Theres seems to be one problem I can't figure out.

--- binomial.cpp
+++ binomial.cpp
@@ -1,69 +1,49 @@
 #include "binomial.h"
 
-Binomial:: Binomial(float cf1,float cf2, int pwr1, int pwr2){
-  coficient1 = cf1;
-  coficient2 = cf2;
-  if (pwr1 < 1)
-       pwr1=1;
-  if (pwr2 < 1)
-       pwr2=1;
-  powr1 = pwr1;
-  powr2 = pwr2;
-}
+ Binomial::Binomial(double c1, int p1, double c2, int p2) : a{c1}, b{c2}, p{p1}, q{p2} 
+ {
+   if (p1 < 1)p = 1;
+   if (p2 < 1)q = 1;
+ }
 
-float Binomial:: GetCoefficient(float idx) {
-  if (idx==1)
-    return coficient1;
-  if (idx==2)
-    return coficient2;
-  else
-    return -1;
-}
-
-int Binomial:: GetPower(int idx, int pwr) {
-  if (idx==1)
-    return pwr1;
-  if (idx==2)
-    return pwr2;
-    else 
-    return -1;
-}
-
-
-int Binomial:: SetPower(int idx, int pwr){
-if (pwr <= 0) 
-   pwr = 1;
- if(idx == 1) 
-    powr1 = pwr;
- if(idx == 2) 
-    powr2 = pwr;
-  else 
-  return 0;
-  return -1;
-}
-
-void Binomial::Add (const Binomial ther){
-  if((ther.GetPower(1)==powr1&&(ther.GetPower(2)==powr2)){
-  coficient1 += ther.GetCoefficient(2);
-  coficient2 += ther.GetCoefficient(1);
-  return 0;
+  double Binomial::GetCoefficient(int i){
+    if (i == 1) return a;
+    if (i == 2) return b;
+    return -1.0;
   }
-  else if ((other.GetPower(2)== powr1) &&(ther.GetPower(1)==powr2)){
-    coficient1 += ther.GetCoefficient(2);
-    coficient2 += ther.GetCoefficient(1);
+
+  int Binomial::GetPower(int i){
+    if (i == 1) return p;
+    if (i == 2) return q;
+    return -1;
+  }
+
+  int Binomial::SetPower(int i, int e){
+    if (e < 1) e = 1;
+    switch(i){
+      case 1:
+        p = e;
+        return 0;
+      case 2:
+        q = e;
+        return 0;
+    }
+    return -1;
+  }
+
+  int Binomial::Add(const Binomial& rhs){
+    if (!((p == rhs.p) && (q == rhs.q)))
+    return -1;
+    a += rhs.a;
+    b += rhs.b;
     return 0;
   }
-  else 
-     return -1;
+  void Binomial::Multiply(double s){
+    a *= s;
+    b *= s;
   }
-  void Binomial:: Multiply (float g){
-    coficient1 *= g;
-    coficient2 *= g;
-  }
-  
-  void Binomial:: Multiply(float coficient, int pwr){
-    coficient1 *= coficient;
-    coficient2 *= coficient;
-    powr1 += pwr;
-    powr2 += pwr;
+  void Binomial::Multiply(double c, int e){
+   Multiply(c);
+    p += e;
+    b += e;
   }
--- binomial.h
+++ binomial.h
@@ -1,20 +1,12 @@
-#ifndef BINOMIAL_H
-#define BINOMIAL_H
-
 class Binomial{
-  float coficient1;
-  float coficient2;
-  int powr1;
-  int powr2;
+  double a,b;
+  int p,q;
   public:
-
-  Binomial(float cf1 = 1.0, float cf2 = 1.0, int pwr1 = 1, int pwr2 = 1);
-  float GetCoefficient(float idx);
-  int GetPower(int idx, int pwr);
-  int SetPower(int idx, int pwr);
-  void Add(const Binomial ther);
-  void Multiply(float g);
-  void Multiply(float coficient, int pwr);
+  Binomial(double = 1.0, int = 1, double  = 1.0, int = 1);
+  double GetCoefficient(int);
+  int GetPower(int);
+  int SetPower(int, int);
+  int Add(const Binomial&);
+  void Multiply(double s);
+  void Multiply(double c, int e);
 };
-
-#endif
